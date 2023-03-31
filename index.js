1c3cdc093a67f3c5809bbf05f7f4cedfe6c4065e
StevenMo <stevenmolvin@gmail.com>
2023-03-31 18:14:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let letters = word.split("");
+  let inverse = letters.reverse();
+  let newWord = inverse.join("");
+  if (word == newWord) {
+    return true;
+  }else {
+    return false;
+  }
 }
-
+isPalindrome("mom");
 /* 
-  Add your pseudocode here
+  Pseudocode
+  Initialize a function 
+  Assign a method which splits the letters of a word,
+  reverses the spelling order and rejoins the letters to a variable.
+  If the result of the method is equal to a word, return "true"
+  otherwise return false.
 */
 
 /*
-  Add written explanation of your solution here
+  Solution
+  The function "isPalindrome" takes a string as an argument(word).
+  A variable, "letters", is declared and assigned a value which
+  takes a word then uses"split" method to create an array of its letters.
+  Another variable, "inverse" is declared and assigned a value which uses
+  the "reverse" method to invert the order of the elements in the letters 
+  array which was formed prior.
+  Yet another variable, "newWord", is declared and is assigned a value which 
+  takes the inverted array from the inverse variable then uses the "join" 
+  method to merge the inverted array elements into a single string.
+  Finally the if statement is used to return "true" if the value of the newWord
+  variable is strictly equal to the original word or "false" if it is not.
+  The required boolean result is returned upon calling the "isPalindrome" function
+  and passing a string to it. 
 */
 
 // You can run `node index.js` to view these console logs
