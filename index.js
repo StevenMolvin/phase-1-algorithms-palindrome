bd274e911cc1bb00c665ddbf43fc76d3c603bd9e
StevenMo <stevenmolvin@gmail.com>
2023-04-03 00:52:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -9,29 +9,33 @@ function isPalindrome(word) {
   }
 }
 isPalindrome("mom");
+
 /* 
   Pseudocode
-  Initialize a function 
-  Assign a method which splits the letters of a word,
-  reverses the spelling order and rejoins the letters to a variable.
-  If the result of the method is equal to a word, return "true"
+  1. Initialize a function 
+  2. Assign methods which (splits the letters of a word and creates
+  an array which has the individual letters as the elements,
+  reverses the order of the elements in the array and rejoins the 
+  elements in the array to a single string) to variables.
+  3. If the result of the final method is equal to a word, return "true"
   otherwise return false.
+  4. Call the function.
 */
 
 /*
   Solution
-  The function "isPalindrome" takes a string as an argument(word).
-  A variable, "letters", is declared and assigned a value which
+  1. The function "isPalindrome" takes a string as an argument(word).
+  2. A variable, "letters", is declared and assigned a value which
   takes a word then uses"split" method to create an array of its letters.
-  Another variable, "inverse" is declared and assigned a value which uses
+  3. Another variable, "inverse" is declared and assigned a value which uses
   the "reverse" method to invert the order of the elements in the letters 
   array which was formed prior.
-  Yet another variable, "newWord", is declared and is assigned a value which 
+  4. Yet another variable, "newWord", is declared and is assigned a value which 
   takes the inverted array from the inverse variable then uses the "join" 
   method to merge the inverted array elements into a single string.
-  Finally the if statement is used to return "true" if the value of the newWord
+  5. Finally the if statement is used to return "true" if the value of the newWord
   variable is strictly equal to the original word or "false" if it is not.
-  The required boolean result is returned upon calling the "isPalindrome" function
+  6. The required boolean result is returned upon calling the "isPalindrome" function
   and passing a string to it. 
 */
 
